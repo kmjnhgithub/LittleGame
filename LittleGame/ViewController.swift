c95cdee478df8ef1516b77cb51353579911db062
Mike <kmjnh2016@gmail.com>
2025-05-14 14:31:55 +0800

    Initial Commit

---
+++ LittleGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittleGame
+//
+//  Created by mike liu on 2025/5/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LittleGameTests/LittleGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  LittleGameTests.swift
+//  LittleGameTests
+//
+//  Created by mike liu on 2025/5/14.
+//
+
+import Testing
+@testable import LittleGame
+
+struct LittleGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
